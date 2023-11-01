2b9b798ecbffa4b957798232dab2bafedbf857ec
vamsi <vamsikrishnagannamaneni@gmail.com>
2023-11-01 09:26:53 -0500

    Fix configuration issue

--- trade_config.py
+++ trade_config.py
@@ -10,6 +10,6 @@ short_term_pivot_period = 1
 #long_term
 
  #           pivot_st = PivotSuperTrendConfiguration(period = trade_config.short_term_period, atr_multiplier = trade_config.short_term_atr_multiplier, pivot_period = trade_config.short_term_pivot_period)
-long_term_period = 1   #2
-long_term_atr_multiplier = 1  #2.8
-long_term_pivot_period = 1   #2
+long_term_period = 3  #2
+long_term_atr_multiplier = 3 #2.8
+long_term_pivot_period = 3  #2
